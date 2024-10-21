3ea620739b9cad9a5e6fe17b3cfb5be4b0c4d732
atakowiec <mat.cieszczyk@gmail.com>
2024-10-21 20:34:26 +0200

    feat: init frontend

---
+++ frontend/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module pl.pollub.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens pl.pollub.frontend to javafx.fxml;
+    exports pl.pollub.frontend;
+}
